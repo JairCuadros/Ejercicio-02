f28d9f864eba5e6a3a642222c92a85c335e838bf
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-10-04 17:32:57 -0500

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,11 @@
+﻿int suma = 0;
+int f = 1000;
+int a = 0;
+Console.WriteLine("===============================================");
+for (int i = 1; i < f; i+=3)
+{
+    a += 1;
+    suma= suma+3;
+    Console.WriteLine($"Divisor de Omar Orlandini {a}: {suma}");
+}
+Console.WriteLine("===============================================");
